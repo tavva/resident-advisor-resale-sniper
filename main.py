e163f4181033e323637e5a8d8089fe08afc3e80c
Ben Phillips <ben@thread.com>
2017-10-25 15:37:36 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import time
+import datetime
+import requests
+import subprocess
+
+from lxml import html
+
+url = sys.argv[1]
+ticket_ids = sys.argv[1:]
+
+while True:
+    response = requests.get(url)
+    root = html.document_fromstring(response.content)
+
+    tickets_with_resale = root.xpath('//li[@id="tickets"]/ul/li[@data-resale-tickets-available > 0]')
+
+    if len(tickets_with_resale) > 0:
+        print("%s: Tickets available for %s" % (str(datetime.datetime.now()), url))
+        subprocess.Popen(['notify-send', "Tickets available"])
+
+    time.sleep(10)
