5a7ecfee84e8189a4e183305208fdf73e1489db0
Ben Phillips <ben@thread.com>
2023-01-05 09:27:52 +0000

    Update to use Clicksend over Twilio, and to cope with new RA site

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import argparse
 import datetime
 import requests
 import subprocess
+import clicksend_client
+from clicksend_client import SmsMessage
+from clicksend_client.rest import ApiException
 
-from lxml import html
-from twilio.rest import Client
+import urllib.request
+import urllib.parse
+
+from fake_useragent import UserAgent
 
 parser = argparse.ArgumentParser(
     description="Poll a given Resident Advisor URL and notify if resale tickets are available.",
@@ -15,33 +20,49 @@ parser = argparse.ArgumentParser(
 parser.add_argument('url', help="Resident Advisor event page URL")
 args = parser.parse_args()
 
-TWILIO_SID = os.getenv('TWILIO_SID')
-TWILIO_TOKEN = os.getenv('TWILIO_TOKEN')
+CLICKSEND_USERNAME = os.getenv('CLICKSEND_USERNAME')
+CLICKSEND_PASSWORD = os.getenv('CLICKSEND_PASSWORD')
+PHONE_NUMBER = os.getenv('PHONE_NUMBER')
 
-FROM_PHONE_NUMBER = '+447481342362'
+headers = {'User-Agent': UserAgent().chrome}
 
-PHONE_NUMBER = os.getenv('RA_SNIPER_PHONE_NUMBER')
+# Configure HTTP basic authorization: BasicAuth
+configuration = clicksend_client.Configuration()
+configuration.username = CLICKSEND_USERNAME
+configuration.password = CLICKSEND_PASSWORD
 
-client = Client(TWILIO_SID, TWILIO_TOKEN)
+# create an instance of the API class
+api_instance = clicksend_client.SMSApi(clicksend_client.ApiClient(configuration))
+
+def send_sms(message):
+    sms_message = SmsMessage(
+        source="python",
+        body="This is a tst",
+        to=PHONE_NUMBER,
+    )
+
+    sms_messages = clicksend_client.SmsMessageCollection(messages=[sms_message])
+
+    try:
+        # Send sms message(s)
+        api_response = api_instance.sms_send_post(sms_messages)
+        print(api_response)
+    except ApiException as e:
+        print("Exception when calling SMSApi->sms_send_post: %s\n" % e)
 
 text_sent = False
-
 while True:
-    response = requests.get(args.url)
-    root = html.document_fromstring(response.content)
+    response = requests.get(args.url, headers=headers)
 
-    tickets_with_resale = root.xpath('//li[@id="tickets"]/ul/li[@data-resale-tickets-available > 0]')
+    tickets_available = not "Check back to purchase tickets as they become available." in response.text
 
-    if len(tickets_with_resale) > 0:
-        print("%s: Tickets available for %s" % (str(datetime.datetime.now()), url))
-        subprocess.Popen(['notify-send', "Tickets available"])
+    if tickets_available:
+        print("%s: Tickets available for %s" % (str(datetime.datetime.now()), args.url))
 
         if not text_sent:
-            message = client.messages.create(
-                to=PHONE_NUMBER,
-                from_=FROM_PHONE_NUMBER,
-                body="Tickets available! %s" % url,
-            )
+            send_sms("Tickets available! %s" % args.url)
             text_sent = True
+    else:
+        print("%s: Tickets not available." % (str(datetime.datetime.now()),))
 
-    time.sleep(10)
+    time.sleep(20)
